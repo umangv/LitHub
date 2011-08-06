96141b835f8467cc0184e24531d443e2fd410d6c
Umang Varma <umang.me@gmail.com>
2011-08-06 11:33:52 -0500

    Put new configuration options in example file

--- lithub_config-example.py
+++ lithub_config-example.py
@@ -1,1 +1,10 @@
 LITHUB_ROOT = "/home/path/to/your/project/root/directory"
+
+# There are various ways to test/use email. Instructions at
+# https://docs.djangoproject.com/en/1.3/topics/email/
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = '/home/path/to/store/emails/for/debug/'
+
+# To be used by django-registration for registration
+ACCOUNT_ACTIVATION_DAYS = 7
+DEFAULT_FROM_EMAIL="test <test@example.com>"
