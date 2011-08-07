42ac13a8daecac825638729f0bd59df28872a2fc
Umang Varma <umang.me@gmail.com>
2011-08-07 18:55:16 -0500

    Added link to privacy policy.

--- fbconnect/urls.py
+++ fbconnect/urls.py
@@ -28,5 +28,5 @@ urlpatterns = patterns('fbconnect.views',
     url(r'^change_pass/redirect/$', 'change_pass_redir'), 
     url(r'^change_pass/$', 'change_pass'), 
     url(r'^privacy/$', TemplateView.as_view(
-        template_name="fbconnect/privacy.html")), 
+        template_name="fbconnect/privacy.html"), name="fbconnect_privacy"), 
 )
